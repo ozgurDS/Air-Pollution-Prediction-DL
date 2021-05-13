82f2f6aa67d0819245294eb7cf74ae274a8d6141
Özgür Hacımustafaoğlu <73536286+ozgurDS@users.noreply.github.com>
2021-05-13 07:16:14 +0300

    Create LSTM.py

---
+++ LSTM.py
@@ -0,0 +1,146 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import keras
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
+from keras import layers,callbacks
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from datetime import datetime
+
+# load data
+data_path = "../input/pollution/parx_pollution.csv"
+dataset = pd.read_csv(data_path,  parse_dates = [['year', 'month', 'day', 'hour']], index_col=0)
+dataset.drop('No', axis=1, inplace=True)
+# manually specify column names
+dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
+dataset.index.name = 'date'
+# mark all NA values with the mean value
+dataset['pollution'].fillna(int(dataset.pollution.mean()), inplace=True)
+# drop the first 24 hours
+dataset = dataset[24:]
+# summarize first 5 rows
+print(dataset.head(5))
+# save to file
+
+# load dataset
+values = dataset.values
+# specify columns to plot
+groups = [0, 1, 2, 3, 5, 6, 7]
+i = 1
+# plot each column
+pyplot.figure()
+for group in groups:
+	pyplot.subplot(len(groups), 1, i)
+	pyplot.plot(values[:, group])
+	pyplot.title(dataset.columns[group], y=0.5, loc='right')
+	i += 1
+pyplot.show()
+
+# prepare data for lstm
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+
+values = dataset.values
+# integer encode direction
+encoder = LabelEncoder()
+values[:,4] = encoder.fit_transform(values[:,4])
+# ensure all data is float
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# frame as supervised learning
+reframed = series_to_supervised(scaled, 1, 1)
+# drop columns we don't want to predict
+reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
+print(reframed.head())
+
+# split into train and test sets
+values = reframed.values
+n_train_hours = 365 * 24
+train = values[:n_train_hours, :]
+test = values[n_train_hours:, :]
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+
+# define early stopping
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=40, # how many epochs to wait before stopping
+    restore_best_weights=True,
+)
+
+# design network
+model = Sequential()
+model.add(layers.LSTM(72, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(1))
+
+# define optimizer and loss metric
+opt=keras.optimizers.Adam(lr=0.001, decay=1e-6)
+model.compile(loss='mae', optimizer=opt)
+# fit network
+history = model.fit(train_X, train_y, 
+                    epochs=200,
+                    batch_size=72, 
+                    validation_data=(test_X, test_y),
+                    callbacks=[early_stopping],
+                    verbose=2, 
+                    shuffle=False # This is important when working with time series 
+                   )
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
+# make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+
+print(dataset.pollution.describe())
+print('Test RMSE: %.3f' % rmse)
